6356f175813507c98b867d62e7465b79c831ef01
Adesh Gupta <adesh02092000@gmail.com>
2022-02-17 18:14:19 +0530

    Moved the task from the selected pointer, instead of top-left corner(by-default)

--- dragNdrop.js
+++ dragNdrop.js
@@ -4,13 +4,19 @@ export default function setup() {
   addGlobalEventListener("mousedown", "[data-draggable]", e => {
     const selectedItem = e.target
     const itemClone = selectedItem.cloneNode(true)
+    const originalRect = selectedItem.getBoundingClientRect() // gives the dimensions of the item
+    const offset = {
+      x: e.clientX - originalRect.left,
+      y: e.clientY - originalRect.top,
+    }
+    itemClone.style.width = `${originalRect.width}px` // Make sure the dimensions of clone remains the same
     itemClone.classList.add("dragging")
-    positionClone(itemClone, e)
+    positionClone(itemClone, e, offset)
     document.body.append(itemClone)
     selectedItem.classList.add("hide")
 
     const mouseMoveFunc = e => {
-      positionClone(itemClone, e)
+      positionClone(itemClone, e, offset)
     }
     document.addEventListener("mousemove", mouseMoveFunc)
 
@@ -27,7 +33,7 @@ export default function setup() {
   })
 }
 
-function positionClone(itemClone, e) {
-  itemClone.style.top = `${e.clientY}px`
-  itemClone.style.left = `${e.clientX}px`
+function positionClone(itemClone, e, offset) {
+  itemClone.style.top = `${e.clientY - offset.y}px`
+  itemClone.style.left = `${e.clientX - offset.x}px`
 }
