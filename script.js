ad8e8ad61763553d1c98d189e14356f134813e05
Adesh Gupta <adesh02092000@gmail.com>
2022-02-17 20:58:57 +0530

    Note

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ setup(onDragComplete)
 function onDragComplete(dataObj) {
   console.log(dataObj)
 }
+
+// At this point we don't need to worry about how the drag and drop functionality works
+// all we care about is that the onDragComplete function is called when ever there is a
+// successful item dragged.
