f628e21816aebb95389175af375be2fa7789cb88
Adesh Gupta <adesh02092000@gmail.com>
2022-02-17 18:04:04 +0530

    Added basic dragging

--- dragNdrop.js
+++ dragNdrop.js
@@ -4,28 +4,30 @@ export default function setup() {
   addGlobalEventListener("mousedown", "[data-draggable]", e => {
     const selectedItem = e.target
     const itemClone = selectedItem.cloneNode(true)
-    // Hide the selected item, from the original position
+    itemClone.classList.add("dragging")
+    positionClone(itemClone, e)
+    document.body.append(itemClone)
     selectedItem.classList.add("hide")
 
     const mouseMoveFunc = () => {
       console.log("Mouse move")
     }
-    // Need to add the move and up event listeners on the document since we can move the task anywhere,
     document.addEventListener("mousemove", mouseMoveFunc)
 
-    // remove the move event listener, once the mouse is up
-    // Also run the up listener once
     document.addEventListener(
       "mouseup",
       e => {
         document.removeEventListener("mousemove", mouseMoveFunc)
-        selectedItem.classList.remove("hide") // add the task once the mouse is up
+        selectedItem.classList.remove("hide")
+        itemClone.remove() // remove the clone one the mouse is up, so that they don't stay in the air(dragged position)
         console.log("Mouse up")
       },
-      { once: true } // Runs the event listener once and then removes it.
+      { once: true }
     )
   })
 }
 
-// Added the move and up event listners inside the mousedown, since we only want to track move and up,
-// after a draggable is click (mouse down)
+function positionClone(itemClone, e) {
+  itemClone.style.top = `${e.clientY}px`
+  itemClone.style.left = `${e.clientX}px`
+}
