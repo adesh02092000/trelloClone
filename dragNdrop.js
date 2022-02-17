47ee14dea148a06339111e4f877402a2e6c14fd2
Adesh Gupta <adesh02092000@gmail.com>
2022-02-17 17:46:27 +0530

    Added mouse down and up listeners

--- dragNdrop.js
+++ dragNdrop.js
@@ -1,12 +1,24 @@
-// There are three steps to drag n drop
-// 1. Click the task (Mouse down)
-// 2. Move the task (Mouse move)
-// 3. Leave the task (Mouse up)
-// All luckily Js has all these Event handlers
 import addGlobalEventListener from "./utils/addGlobalEventListener"
 
 export default function setup() {
   addGlobalEventListener("mousedown", "[data-draggable]", e => {
     console.log("Mouse Down")
+
+    const mouseMoveFunc = () => {
+      console.log("Mouse move")
+    }
+    // Need to add the move and up event listeners on the document since we can move the task anywhere,
+    document.addEventListener("mousemove", mouseMoveFunc)
+
+    // remove the move event listener, once the mouse is up
+    // Also run the up listener once
+    document.addEventListener(
+      "mouseup",
+      e => {
+        document.removeEventListener("mousemove", mouseMoveFunc)
+        console.log("Mouse up")
+      },
+      { once: true }
+    )
   })
 }
