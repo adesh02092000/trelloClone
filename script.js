b078e1388fb2c7824f8386407cb1bf79242c0484
Adesh Gupta <adesh02092000@gmail.com>
2022-02-18 13:42:17 +0530

    rendered the task from JS

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const STORAGE_PREFIX = "TRELLO_CLONE"
 const LANES_STORAGE_KEY = `${STORAGE_PREFIX}-lanes`
 const DEFAULT_LANES = {
   backlog: [{ id: uuidV4(), text: "Create your first task" }],
-  working: [],
+  doing: [],
   done: [],
 }
 
 const lanes = loadLanes()
 
-console.log(lanes)
+renderTasks()
 
 setup(onDragComplete)
 
@@ -22,4 +22,28 @@ function onDragComplete(dataObj) {
 function loadLanes() {
   const lanesJson = localStorage.getItem(LANES_STORAGE_KEY)
   return JSON.parse(lanesJson) || DEFAULT_LANES
+}
+
+function renderTasks() {
+  Object.entries(lanes).forEach(obj => {
+    const laneId = obj[0]
+    const tasks = obj[1]
+    const lane = document.querySelector(`[data-lane-id="${laneId}"]`)
+    tasks.forEach(task => {
+      const taskElement = createTaskElement(task)
+      lane.append(taskElement)
+    })
+  })
+}
+
+// To loop through objects, use Object.entries()
+//  obj = { a : 1, b : 2} , Object.entries(obj) ==> [[a, 1], [b, 2]]
+
+function createTaskElement(task) {
+  const element = document.createElement("div")
+  element.id = task.id
+  element.innerText = task.text
+  element.classList.add("task")
+  element.dataset.draggable = true
+  return element
 }
